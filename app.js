a19f1053ec8119a8a865f161e0b0c9776f8f1393
mtpullin <mtpullin@gmail.com>
2021-07-13 14:14:36 -0400

    added prompt

--- app.js
+++ app.js
@@ -2,3 +2,21 @@ const express = require('express')
 const PORT = provess.env.PORT || 3001;
 const routes = require('./routes')
 const db = require('./db/connection')
+const inq = require('inquirer')
+
+const prompt = () => {
+    return inq.prompt ({
+        type: 'list',
+        name: 'options',
+        message: 'What would you like to do?',
+        choices: [
+            'view all departments',
+            'view all roles',
+            'view all employees',
+            'add a department',
+            'add a role',
+            'add an employee',
+            'update employee role'
+        ]
+    })
+}
