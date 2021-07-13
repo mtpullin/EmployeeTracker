9fa7f12c7b26c003f0b539bf5786d3b3731416dd
mtpullin <mtpullin@gmail.com>
2021-07-13 10:30:41 -0400

    adjusted to remove visible password

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,13 @@
 const mysql= require('mysql2');
 const secret = require('hide-secrets')
+require('dotenv').config()
+
 const db = mysql.createConnection(
     {
         host: 'localhost',
-        user: 'root',
-        password: '1darkness',
-        database: 'employeedatabase'
+        user: process.env.DB_USER,
+        password: process.env.DB_PW,
+        database: process.env.DB_NAME
     },
     console.log('Connected to the Employee database.')
 )
